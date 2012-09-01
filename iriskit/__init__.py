14ddb1d92f0587828e054bc20a2ee696cc17ef45
Keith Gaughan <k@stereochro.me>
2012-09-01 22:18:51 +0100

    Record metadata in one spot only; read requirements smartly.

--- iriskit/__init__.py
+++ iriskit/__init__.py
@@ -1,3 +1,7 @@
 """
 IRIS client (and eventually server) library.
 """
+
+__version__ = '0.1.0'
+__author__ = 'Keith Gaughan'
+__email__ = 'k@stereochro.me'
--- setup.py
+++ setup.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import re
+
+
+def read(filename):
+    with open(filename, 'r') as fh:
+        return fh.read()
+
+
+def get_metadata(module_path):
+    """Extract the metadata from a module file."""
+    matches = re.finditer(
+        r"^__(\w+?)__ *= *'(.*?)'$",
+        read(module_path),
+        re.MULTILINE)
+    return dict(
+        (match.group(1), match.group(2).decode('unicode_escape'))
+        for match in matches)
+
+
+def read_requirements(requirements_path):
+    """Read a requirements file, stripping out the detritus."""
+    requirements = []
+    with open(requirements_path, 'r') as fh:
+        for line in fh:
+            line = line.strip()
+            if line != '' and not line.startswith(('#', 'svn+', 'git+')):
+                requirements.append(line)
+    return requirements
+
+
+META = get_metadata('iriskit/__init__.py')
+
 
 setup(
     name='iriskit',
-    version='0.1.0',
+    version=META['version'],
     description='Internet Registry Information Service (IRIS) client',
-    long_description=open('README').read(),
+    long_description=read('README'),
     url='https://github.com/kgaughan/iriskit/',
     license='MIT',
     packages=find_packages(exclude='tests'),
     zip_safe=True,
-    install_requires=[line.rstrip() for line in open('requirements.txt')],
+    install_requires=read_requirements('requirements.txt'),
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -25,6 +57,6 @@ setup(
         'Topic :: System :: Networking',
     ],
 
-    author='Keith Gaughan',
-    author_email='k@stereochro.me'
+    author=META['author'],
+    author_email=META['version']
 )
